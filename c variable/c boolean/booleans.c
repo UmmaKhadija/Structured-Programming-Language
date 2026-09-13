92926751573013a8422e1b439a6c1e491837bf46
UmmaKhadija <ummakhadiza455@gmail.com>
2026-09-13 23:36:05 +0600

    c boolean

---
+++ c variable/c boolean/booleans.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    bool isprogrammingfun = true;
+    bool fishistasty = false;
+    prinf("%d",isprogrammingfun);
+    printf("%d",fishistasty);
+    return 0;
+}
