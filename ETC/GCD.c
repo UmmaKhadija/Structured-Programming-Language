9bf3fd6dbd1cc0e650d9c6a7359cd2e147dbae24
UmmaKhadija <ummakhadiza455@gmail.com>
2026-09-24 07:37:16 +0600

    Implement GCD calculation using Euclidean algorithm

--- ETC/GCD.c
+++ ETC/GCD.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    int a, b, temp;
+    printf("Enter two integers numbers: ");
+    scanf("%d %d", &a, &b);
+
+    int originalA = a, originalB = b;
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("GCD of %d and %d = %d\n", originalA, originalB, a);
+    return 0;
+}
